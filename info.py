bb92a480c5f1f65ecad9d96e2f78e85b0da839d9
ak4422920 <ak4422920@gmail.com>
2025-05-01 15:20:48 +0530

    Update info.py

--- info.py
+++ info.py
@@ -60,7 +60,7 @@ AUTH_REQ_CHANNEL = int(environ.get('AUTH_REQ_CHANNEL', '-1001785093771'))
 IS_VERIFY = is_enabled('IS_VERIFY', True)
 LOG_VR_CHANNEL = int(environ.get('LOG_VR_CHANNEL', '-1002453651327')) #Verification Channel Id 
 LOG_API_CHANNEL = int(environ.get('LOG_API_CHANNEL', '-1002365383608')) #If Anyone Set Your Bot In Any Group And Set Shortner In That Group Then In This Channel The All Details Come
-VERIFY_IMG = environ.get("VERIFY_IMG", "https://telegra.ph/file/9ecc5d6e4df5b83424896.jpg")
+VERIFY_IMG = environ.get("VERIFY_IMG", "https://envs.sh/leq.jpg")
 
 TUTORIAL = environ.get("TUTORIAL", "https://t.me/how_to_open_link_ak/10")
 TUTORIAL_2 = environ.get("TUTORIAL_2", "https://t.me/how_to_open_link_ak/10")
